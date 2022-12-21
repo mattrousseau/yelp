3127dfc45b8da221e78bbd50059dcf3df6144c70
Matthieu Rousseau <matthieurousseau86@gmail.com>
2022-12-21 22:30:51 +0000

    Alembic starter

--- yelp/main.py
+++ yelp/main.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI, Depends
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from yelp import schemas, models
 from yelp.database import SessionLocal
 
 app = FastAPI()
-
-db = [{"name": "Chez Justine", "cuisine_style": "French", "city": "Paris"},
-      {"name": "L'Alicheur", "cuisine_style": "Asian", "city": "Paris"}]
 
 def get_db():
     """Helper function which opens a connection to the database and also manages closing the connection
@@ -25,16 +23,23 @@ def get_restaurants(db: Session = Depends(get_db)):
     return db.query(models.Restaurant).all()
 
 @app.get("/restaurants/{restaurant_id}", response_model=schemas.Restaurant)
-def get_restaurant(restaurant_id: int):
-    return db[restaurant_id - 1]
+def get_restaurant(restaurant_id: int, db: Session = Depends(get_db)):
+    db_restaurant = db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
+    if db_restaurant is None:
+        raise HTTPException(status_code=404, detail="Restaurant not found")
+    return db_restaurant
 
 @app.delete("/restaurants/{restaurant_id}")
-def delete_restaurant(restaurant_id: int):
-    db.pop(restaurant_id - 1)
+def delete_restaurant(restaurant_id: int, db: Session = Depends(get_db)):
+    db_restaurant = db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
+    if db_restaurant is None:
+        raise HTTPException(status_code=404, detail="Restaurant not found")
+    db.delete(db_restaurant)
+    db.commit()
     return {}
 
 @app.post("/restaurants", response_model=schemas.Restaurant)
-def create_restaurant(restaurant: schemas.Restaurant, db: Session = Depends(get_db)):
+def create_restaurant(restaurant: schemas.RestaurantCreate, db: Session = Depends(get_db)):
     new_restaurant = models.Restaurant(**restaurant.dict())
     db.add(new_restaurant)
     db.commit()
